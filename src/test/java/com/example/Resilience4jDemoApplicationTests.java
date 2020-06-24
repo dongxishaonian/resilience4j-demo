ea5e59cc381753183ca830bff27ec5574b87ee1e
chengang <837718548@qq.com>
2020-06-24 23:17:04 +0800

    忽略主单元测试

--- src/test/java/com/example/Resilience4jDemoApplicationTests.java
+++ src/test/java/com/example/Resilience4jDemoApplicationTests.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@Disabled
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class Resilience4jDemoApplicationTests {
 
